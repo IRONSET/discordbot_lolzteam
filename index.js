0e1abce68c33e64b55809d39cdb943b7c44a260e
IRONSET <53252322+IRONSET@users.noreply.github.com>
2019-07-24 21:30:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-client.login("NDIzMjM4OTcxMDExNzYwMTMx.DYnbhw.eRF3BeObgx6BAkFi8PoeLILFuBU");
+client.login("NTk5Mjc2ODAyMTMyOTM0NjU2.XTieQA.t5rMHsh9af8rKJoPGfVXJ-NHmB0");
 
 console.log("start");
 
