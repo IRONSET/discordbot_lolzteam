3394bb96c52b89f2857e7bc35852311949d3d693
IRONSET <53252322+IRONSET@users.noreply.github.com>
2019-07-25 11:42:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-client.login("NTk5Mjc2ODAyMTMyOTM0NjU2.XTieQA.t5rMHsh9af8rKJoPGfVXJ-NHmB0");
+client.login("NjAzODI3Mjc4MDAyMTI2ODQ4.XTlEww.hDO73paayZyQfc1nWljMEbKWuHY");
 
 console.log("start");
 
 client.on('message', (message) => {
     if(message.content == "бот")
     {
-        message.channel.send("#моя_статья");
+        message.channel.send("ШО НАДО, ЛОХ?");
     }
 });
